f8061508233f9889e7887b32e9f6fa55567c02c3
Facundo Pascual <facundo.pascual@nobu.com>
2026-03-04 03:27:03 -0300

    fix de output

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',        // <- clave: genera /out estático
+  images: {
+    unoptimized: true,     // <- necesario para export estático si usás next/image
+  },
   typescript: {
     ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
   },
 }
 
